1fd5fb1fbc05ecd427398988aee4769220dc9db7
Dmitry <davidowski7777@gmail.com>
2020-04-14 22:32:36 +0300

    feat: add code subtask345

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -2,8 +2,18 @@ package subtask3
 
 class Abbreviation {
 
-    // TODO: Complete the following function
+
     fun abbreviationFromA(a: String, b: String): String {
+
+        var aToUpper = a.toUpperCase()
+        var result = aToUpper
+
+        for (i in b.indices) aToUpper = aToUpper.substringAfter(b[i], "NO")
+
+        if (aToUpper != "NO") return "YES"
+        else return "NO"
+
+
         throw NotImplementedError("Not implemented")
     }
 }
--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -2,8 +2,27 @@ package subtask6
 
 class Fibonacci {
 
-    // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
+
+        var smallerFibNumber = 0
+        var largerFibNumber = 1
+
+        val outputArray: IntArray = intArrayOf(0, 0 ,0)
+
+        while(smallerFibNumber * largerFibNumber < n){
+            val largerValue = largerFibNumber
+            largerFibNumber += smallerFibNumber
+            smallerFibNumber = largerValue
+        }
+
+        outputArray[0] = smallerFibNumber
+        outputArray[1] = largerFibNumber
+
+        if (smallerFibNumber * largerFibNumber == n) outputArray[2] = 1
+        else outputArray[2] = 0
+
+        return  outputArray
+
         throw NotImplementedError("Not implemented")
     }
 }
